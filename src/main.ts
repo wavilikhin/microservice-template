5af6990bb324e2d844634830d6c4ecaadf4676af
Vladislav Vavilikhin <vavilikhin.v@gmail.com>
2022-09-18 19:50:24 +0300

    feat: adds dontenv-safe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as env from 'dotenv-safe';
+
+env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT);
 }
+
 bootstrap();
