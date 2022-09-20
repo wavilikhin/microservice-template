d0e74fa3952f589aff16bf27b4b26227fb809069
Vladislav Vavilikhin <vavilikhin.v@gmail.com>
2022-09-20 18:15:48 +0300

    chore: moves server to a separate folder

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as env from 'dotenv-safe';
 
-env.config();
+env.config({ path: './../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
