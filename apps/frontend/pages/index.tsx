f746f92ccd6cfb0e0af3128f4acd0ea3b3d30f4e
Vladislav Vavilikhin <vavilikhin.v@gmail.com>
2022-09-26 20:36:44 +0300

    feat: adds dev docker-compose file for frontend app

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -7,49 +7,49 @@ import { grey } from '@ant-design/colors';
 import { usePosts } from 'hooks/usePosts/usePosts';
 
 const Home: NextPage = () => {
-    const { isLoading, isError, isSuccess, data } = usePosts()
+  const { isLoading, isError, isSuccess, data } = usePosts()
 
-    return (
-        <>
-            <Header />
-            <Row gutter={100} justify='center'>
-                <Col span={8}>
-                    <List
-                        header={<Title level={3}>Recent posts:</Title>}
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={item => (
-                            <List.Item
-                                actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">delete</a>]}>
-                                <List.Item.Meta
-                                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                                    title={<a href="https://ant.design">{item.title}</a>}
-                                    description={item.content}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </Col>
+  return (
+    <>
+      <Header />
+      <Row gutter={100} justify='center'>
+        <Col span={8}>
+          <List
+            header={<Title level={3}>Recent posts:</Title>}
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={item => (
+              <List.Item
+                actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">remove</a>]}>
+                <List.Item.Meta
+                  avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  title={<a href="https://ant.design">{item.title}</a>}
+                  description={item.content}
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
 
-                <Col span={6} style={{ padding: '60px' }}>
-                    <Card style={{ width: 300 }}>
-                        <Space direction='vertical' size='large'>
-                            <>
-                                <Space direction='vertical' size='middle'>
-                                    <Title level={3} className='geekblue-2' style={{ color: grey[6] }}>Create new post</Title>
-                                    <Input placeholder='Title' />
-                                    <TextArea rows={4} placeholder='Content' />
-                                </Space>
-                            </>
-                            <Button type="primary" size='middle'>
-                                Send data
-                            </Button>
-                        </Space>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    )
+        <Col span={6} style={{ padding: '60px' }}>
+          <Card style={{ width: 300 }}>
+            <Space direction='vertical' size='large'>
+              <>
+                <Space direction='vertical' size='middle'>
+                  <Title level={3} className='geekblue-2' style={{ color: grey[6] }}>Create new post</Title>
+                  <Input placeholder='Title' />
+                  <TextArea rows={4} placeholder='Content' />
+                </Space>
+              </>
+              <Button type="primary" size='middle'>
+                Create post
+              </Button>
+            </Space>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default Home
